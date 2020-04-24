a5b34d30afe60434166ef0f0a398119c09ce4c17
lauraherrero <lauraherrero36@gmail.com>
2020-04-24 19:29:04 +0200

    Initial commit

---
+++ src/components/pages/foods-index/FoodsIndex.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Container, Table, Spinner } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+class FoodsIndex extends Component {
+
+    constructor () {
+        super()
+        this.state = {foods: undefined }
+    }
+
+    componentDidMount () {
+        fetch('http://react-webinar.herokuapp.com/')
+            .then(response => response.json())
+            .then(foodsArray => this.setState({ foods: foodsArray }))
+            .catch(error => console.log(error))
+    }
+
+    render () {
+
+        return (
+            <Container>
+
+                {
+                    !this.state.foods ?
+
+                        <Spinner animation="border" role="status" className="spinner">
+                            <h3 className="sr-only">Loading...</h3>
+                        </Spinner>
+
+                    :
+                        <>
+                        <h1>Stock de alimentos</h1>
+                        <p>Consulta los detalles de stock, precios y origen de nuestros alimentos.</p>
+
+                        <Table bordered>
+                            <tbody>
+                                {this.state.foods.map(food => 
+                                <tr key={food._id}>
+                                    <td>
+                                        <img src={`/img/${food.img}`} alt={food.name}/>
+                                    </td>
+                                    <td>
+                                        <p>{food.name}</p>
+                                    </td>
+                                    <td>
+                                        <p>Precio: {food.price} | Stock: {food.stock} | Kcal: {food.kcal}</p>
+                                    </td>
+                                    <td>
+                                        <p>Precio: {food.price} € | Stock disponible: {food.stcok} uds.
+                                        <Link className="btn btn-sm btn-outline-dark" to={`/details/${food._id}`}>Ver detalles</Link></p>
+                                    </td>
+                                </tr>
+                                )}
+                            </tbody>
+                        </Table>
+                    </>
+                }
+
+            </Container>
+        )
+    };
+}
+
+
+export default FoodsIndex;
---
+++ src/components/ui/navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
+
+const Navigation = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="md">
+            <Navbar.Brand as="div"><Link to="/">Veggie stock corner</Link></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link as="div"><Link to="/">Inicio</Link></Nav.Link>
+                    <Nav.Link as="div"><Link to="/foods">Alimentos</Link></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+
+export default Navigation;
